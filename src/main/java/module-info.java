9fbcad421e09273ef0f46954290e456801b6870b
Jimena Calvo <jimenacalvosoto13@gmail.com>
2026-04-13 17:14:55 -0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ucr.algoritmos.pg03algoritmos {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ucr.algoritmos.pg03algoritmos to javafx.fxml;
+    exports ucr.algoritmos.pg03algoritmos;
+}
