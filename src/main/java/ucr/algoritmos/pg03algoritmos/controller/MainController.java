ee4254f5447a312914ce64aa114d471fcc477edf
Jimena Calvo <jimenacalvosoto13@gmail.com>
2026-04-15 18:44:55 -0600

    Main

--- src/main/java/ucr/algoritmos/pg03algoritmos/controller/MainController.java
+++ src/main/java/ucr/algoritmos/pg03algoritmos/controller/MainController.java
@@ -1,4 +1,8 @@
 package ucr.algoritmos.pg03algoritmos.controller;
 
 public class MainController {
+    @javafx.fxml.FXML
+    private Canvas canvasBin;
+    @javafx.fxml.FXML
+    private TabPane mainTabs;
 }
