b174aa3640bded58fb9fadb7b7892f82743fd199
Jimena Calvo <jimenacalvosoto13@gmail.com>
2026-04-15 19:42:17 -0600

    Clase 2

--- src/main/java/ucr/algoritmos/pg03algoritmos/controller/MainController.java
+++ src/main/java/ucr/algoritmos/pg03algoritmos/controller/MainController.java
@@ -1,7 +1,11 @@
 package ucr.algoritmos.pg03algoritmos.controller;
 
+import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.*;
+
+import java.math.BigInteger;
+import java.util.List;
 
 public class MainController {
     @javafx.fxml.FXML
@@ -16,15 +20,56 @@ public class MainController {
     @javafx.fxml.FXML
     private Canvas canvasMiller;
     @javafx.fxml.FXML
-    private Spinner spParams;
+    private Spinner<BigInteger> spParams;
     @javafx.fxml.FXML
-    private TableView tableViewBigNumber;
+    private TableView<List<String>> tableViewBigNumber;
     @javafx.fxml.FXML
     private Button btnCleanField;
     @javafx.fxml.FXML
     private TextField txfBigInteger;
     @javafx.fxml.FXML
     private ListView listViewOperations;
+
+    //TAB-1 - ATRIBUTOS INTERNOS
+    BigInteger min = new BigInteger("0");
+    BigInteger max = new BigInteger("1000000000000000000");
+    BigInteger initial = new BigInteger("1000000000000");
+    BigInteger step = new BigInteger("1");
+
+    @FXML
+    public void initialize() {
+        setUpMillerRabin();
+
+    }
+
+    private void setUpMillerRabin() {
+
+        //spParams.setValueFactory(new BigIntegerSpinnerValueFactory(min,max,initial,step));
+        spParams.valueProperty().addListener((obs, oldValue, newValue) -> {
+           txfBigInteger.setText(newValue.toString());
+        });
+
+        btnMillerRabin.setOnAction(e -> runMillerRabin());
+        btnClean.setOnAction(e->reset(1));
+        btnCleanField.setOnAction(e-> txfBigInteger.setText(""));
+    }
+
+    private void runMillerRabin() {
+    }
+
+    private void reset(int i) {
+
+        switch (i) {
+            case 1: //Tab-1
+                listViewOperations.getItems().clear();
+                break;
+
+            case 2 :
+                break;
+        }
+
+
+    }
 
 
 }
