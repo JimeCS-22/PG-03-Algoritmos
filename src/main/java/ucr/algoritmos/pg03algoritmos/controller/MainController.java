f3bb8a4deb88960a5a37fe9ce8164aaa384cfac0
Camila Mariana Montoya Blanco <81707715+CamilaMontoya31@users.noreply.github.com>
2026-04-15 18:54:54 -0600

    namesMillerRabin

--- src/main/java/ucr/algoritmos/pg03algoritmos/controller/MainController.java
+++ src/main/java/ucr/algoritmos/pg03algoritmos/controller/MainController.java
@@ -1,8 +1,28 @@
 package ucr.algoritmos.pg03algoritmos.controller;
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.*;
+
 public class MainController {
     @javafx.fxml.FXML
-    private Canvas canvasBin;
-    @javafx.fxml.FXML
     private TabPane mainTabs;
+
+    @javafx.fxml.FXML
+    private Button btnGenRandom;
+    @javafx.fxml.FXML
+    private Button btnClean;
+    @javafx.fxml.FXML
+    private Button btnMillerRabin;
+    @javafx.fxml.FXML
+    private Canvas canvasMiller;
+    @javafx.fxml.FXML
+    private Spinner spParams;
+    @javafx.fxml.FXML
+    private TableView tableViewBigNumber;
+    @javafx.fxml.FXML
+    private Button btnCleanField;
+    @javafx.fxml.FXML
+    private TextField txfBigInteger;
+    @javafx.fxml.FXML
+    private ListView listViewOperations;
 }
