3a80dfe5dd2a459e4949c899a1564233f13b8f3f
Jimena Calvo <jimenacalvosoto13@gmail.com>
2026-04-20 17:48:11 -0600

    Lista enlazada

--- src/main/java/util/Utility.java
+++ src/main/java/util/Utility.java
@@ -34,4 +34,33 @@ public class Utility {
 
         return formatter.format(big);
     }
+
+    public static String instanceOf(Object a, Object b) {
+        if (a instanceof Integer && b instanceof Integer) return "Integer";
+        if (a instanceof String && b instanceof String) return "String";
+        if (a instanceof Character && b instanceof Character) return "Character";
+        return "Unknown";
+    }
+
+    public static int compare(Object a, Object b) {
+        switch (instanceOf(a, b)) {
+            case "Integer":
+                Integer int1 = (Integer) a;
+                Integer int2 = (Integer) b;
+                return int1 < int2 ? -1 : int1 > int2 ? 1 : 0;
+
+            case "String":
+                String str1 = (String) a;
+                String str2 = (String) b;
+                return str1.compareTo(str2) < 0 ? -1 : str1.compareTo(str2) > 0 ? 1 : 0;
+
+            case "Character":
+                Character ch1 = (Character) a;
+                Character ch2 = (Character) b;
+                return ch1.compareTo(ch2) < 0 ? -1 : ch1.compareTo(ch2) > 0 ? 1 : 0;
+
+
+        }
+        return 2; //Unknown
+    }
 }
