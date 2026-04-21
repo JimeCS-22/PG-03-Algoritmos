6fd6c9877baac504d59edc9b5a968f6dcb83623f
Jimena Calvo <jimenacalvosoto13@gmail.com>
2026-04-20 18:54:06 -0600

    Lista enlazada

--- src/test/java/ucr/algoritmos/pg03algoritmos/model/LinkedListTest.java
+++ src/test/java/ucr/algoritmos/pg03algoritmos/model/LinkedListTest.java
@@ -2,6 +2,8 @@ package ucr.algoritmos.pg03algoritmos.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LinkedListTest {
@@ -12,6 +14,12 @@ class LinkedListTest {
         LinkedList<Integer> linkedList = new LinkedList<>();
         linkedList.add(20);
         linkedList.add(10);
+
+        for (int i = 0; i < 20; i++) {
+
+            linkedList.add(new Random().nextInt(50));
+
+        }
         System.out.println(linkedList);
 
 
