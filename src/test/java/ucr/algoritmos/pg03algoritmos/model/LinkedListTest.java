bac1c12c8bf98ed7df092e8447cb16e4819ad846
Jimena Calvo <jimenacalvosoto13@gmail.com>
2026-04-27 20:22:58 -0600

    Metodos

--- src/test/java/ucr/algoritmos/pg03algoritmos/model/LinkedListTest.java
+++ src/test/java/ucr/algoritmos/pg03algoritmos/model/LinkedListTest.java
@@ -112,6 +112,43 @@ class LinkedListTest {
     @Test
     public void personLinkedListTest(){
 
+        LinkedList<Person> linkedList = new LinkedList<>();
+        linkedList.add(new Student("1", "María", 19, 1.6, 56.5, "C12345"));
+        linkedList.add(new Employee("2", "Carlos", 25, 1.73, 75, "Informático"));
+        linkedList.add(new Student("3", "Mario", 22, 1.75, 76.3, "C67890"));
+        linkedList.add(new Employee("4", "Ana", 30, 1.55, 60, "Profesora"));
+        linkedList.add(new Student("5", "Paola", 21, 1.58, 60, "C85746"));
+        linkedList.add(new Employee("6", "Juan", 32, 1.80, 78, "Paramédico"));
+        System.out.println(linkedList); //mostramos la lista personas
+        System.out.println("_".repeat(50));
+
+        try {
+            System.out.println("List size: "+linkedList.size());
+            System.out.println("Removed first item: "+linkedList.removeFirst());
+
+            Person person = linkedList.get(new Random().nextInt(linkedList.size()));
+            System.out.println("_".repeat(50));
+            System.out.println(
+                    linkedList.contains(person)
+                            ? "The person [" + person + "] exists in the list. " +
+                            "Index: " + linkedList.indexOf(person)
+                            : "The person [" + person + " does not exist in the list"
+            );
+
+            System.out.println("_".repeat(50));
+            //probamos remove
+            if (linkedList.contains(person)) {
+                linkedList.remove(person);
+                System.out.println("The person [" + person + "] has been deleted");
+            }
+
+        } catch (ListException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("_".repeat(50));
+        System.out.println(linkedList); //mostramos nuevamente la lista de personas
+
+
     }
 
 }
