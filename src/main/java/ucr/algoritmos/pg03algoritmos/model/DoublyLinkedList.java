e948aea5796ce2160b2c520fcf9642636ea94538
Jimena Calvo <jimenacalvosoto13@gmail.com>
2026-04-27 18:32:13 -0600

    Metodo addFirst

---
+++ src/main/java/ucr/algoritmos/pg03algoritmos/model/DoublyLinkedList.java
@@ -0,0 +1,115 @@
+package ucr.algoritmos.pg03algoritmos.model;
+
+public class DoublyLinkedList<T> implements List<T>{
+
+    private Node<T> head; //Inicio de la lista
+    private Node<T> tail; //Fin de la lista
+
+    public DoublyLinkedList(Node<T> head, Node<T> tail) {
+        this.head = null;
+        this.tail = null;
+    }
+
+    @Override
+    public int size() throws ListException {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void add(T element) {
+
+    }
+
+    @Override
+    public void addFirst(T element) {
+
+        Node<T> node = new Node<>(element);
+
+        if (isEmpty()) {
+
+            head = tail = node;
+
+        }else{
+
+            node.next = head;
+            head.prev = node;
+            head = node;
+        }
+
+    }
+
+    @Override
+    public void addLast(T element) {
+
+    }
+
+    @Override
+    public void addInSortedList(T element) {
+
+    }
+
+    @Override
+    public void remove(T element) throws ListException {
+
+    }
+
+    @Override
+    public T removeFirst() throws ListException {
+        return null;
+    }
+
+    @Override
+    public T removeLast() throws ListException {
+        return null;
+    }
+
+    @Override
+    public boolean contains(T element) throws ListException {
+        return false;
+    }
+
+    @Override
+    public void sort() throws ListException {
+
+    }
+
+    @Override
+    public int indexOf(T element) throws ListException {
+        return 0;
+    }
+
+    @Override
+    public T getFirst() throws ListException {
+        return null;
+    }
+
+    @Override
+    public T getLast() throws ListException {
+        return null;
+    }
+
+    @Override
+    public T getPrev(T element) throws ListException {
+        return null;
+    }
+
+    @Override
+    public T getNext(T element) throws ListException {
+        return null;
+    }
+
+    @Override
+    public T get(int index) throws ListException {
+        return null;
+    }
+}
